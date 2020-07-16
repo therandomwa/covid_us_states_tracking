ae40743bd6641124ca731b74a62f3e371bb77bac
therandomwa <aijinw@alumni.cmu.edu>
2020-07-16 13:53:19 -0400

    ...

--- source/aijin/census_clean.R
+++ source/aijin/census_clean.R
@@ -1,9 +1,9 @@
-df = read.csv("../Data/census.csv")
+df = read.csv("../Data/census_2019.csv")
 
 colnames(df) = colnames(df) %>% 
   toupper() %>% 
   gsub("NON.HISPANIC", "NH", .) %>% 
-  gsub("AMERICAN.INDIAN.AND.ALASKAN.NATIVE", "AI/AN", .) %>% 
-  gsub("NATIVE.HAWAIIAN.AND.OTHER.PACIFIC.ISLANDER", "NH/PI", .) %>% 
-  gsub("TWO.OR.MORE.RACES", "MULTI", .) %>% 
-write.csv(df, "../Data/census.csv", row.names = F)
+  gsub("AMERICAN.INDIAN.AND.ALASKAN.NATIVE", "AI.AN", .) %>% 
+  gsub("NATIVE.HAWAIIAN.AND.OTHER.PACIFIC.ISLANDER", "NH.PI", .) %>% 
+  gsub("TWO.OR.MORE.RACES", "MULTI", .)
+write.csv(df, "../Data/census_2019_clean.csv", row.names = F)
